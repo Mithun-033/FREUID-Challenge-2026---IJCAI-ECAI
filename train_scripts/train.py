39b1b2e4a4c63d28fece24ad11119e462058f527
Mithun Kannaa <mithunkannaa609@gmail.com>
2026-07-05 15:04:34 +0530

    modulated the eval part

--- train_scripts/train.py
+++ train_scripts/train.py
@@ -14,12 +14,41 @@ from tqdm import tqdm
 import sys
 import json
 
-device =torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-def train_model(epochs):
-    model_con, data_con, train_con= config(), data_config(), train_config()
+def eval_model(model, criterion, val_dataloader, device):
+    model.eval()
+
+    val_loss_accum = 0.0
+    acc_metric = torchmetrics.Accuracy(task="binary").to(device)
+    prec_metric = torchmetrics.Precision(task="binary").to(device)
+    rec_metric = torchmetrics.Recall(task="binary").to(device)
+    acc_metric.reset()
+    prec_metric.reset()
+    rec_metric.reset()
+    with torch.no_grad():
+        for x_val, y_val in val_dataloader:
+            x_val = x_val.to(device, memory_format = torch.channels_last)
+            y_val = y_val.to(device).unsqueeze(1).float()
+            with torch.autocast(device_type="cuda", dtype=torch.bfloat16):
+                output = model(x_val)
+                loss = criterion(output, y_val)
+            val_loss_accum += loss.item()
+            acc_metric.update(output, y_val.int())
+            prec_metric.update(output, y_val.int())
+            rec_metric.update(output, y_val.int())
+
+    avg_val_loss = val_loss_accum / len(val_dataloader)
+    avg_accuracy = acc_metric.compute().item()
+    avg_precision = prec_metric.compute().item()
+    avg_recall = rec_metric.compute().item()
+
+    return avg_val_loss, avg_accuracy, avg_precision, avg_recall
+
+
+def train_model(epochs, model_con, data_con, train_con, device):
+    
     model = Model(model_con).to(device, memory_format = torch.channels_last)
-    # model = torch.compile(model).to(device)
+    model = torch.compile(model).to(device)
 
     optimizer = optim.AdamW(model.parameters(), lr=train_con.lr, weight_decay=train_con.weight_decay, fused = True)
     scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max = epochs * (68352) / data_config.batch_size, eta_min = train_con.min_lr)
@@ -64,37 +93,7 @@ def train_model(epochs):
                     loss_acum = 0.0
 
                 if step % 250 == 0:
-                    model.eval()
-
-                    val_loss_accum = 0.0
-
-                    acc_metric = torchmetrics.Accuracy(task="binary").to(device)
-                    prec_metric = torchmetrics.Precision(task="binary").to(device)
-                    rec_metric = torchmetrics.Recall(task="binary").to(device)
-
-                    acc_metric.reset()
-                    prec_metric.reset()
-                    rec_metric.reset()
-
-                    with torch.no_grad():
-                        for x_val, y_val in val_dataloader:
-                            x_val = x_val.to(device, memory_format = torch.channels_last)
-                            y_val = y_val.to(device).unsqueeze(1).float()
-
-                            with torch.autocast(device_type="cuda", dtype=torch.bfloat16):
-                                output = model(x_val)
-                                loss = criterion(output, y_val)
-
-                            val_loss_accum += loss.item()
-
-                            acc_metric.update(output, y_val.int())
-                            prec_metric.update(output, y_val.int())
-                            rec_metric.update(output, y_val.int())
-
-                    avg_val_loss = val_loss_accum / len(val_dataloader)
-                    avg_accuracy = acc_metric.compute().item()
-                    avg_precision = prec_metric.compute().item()
-                    avg_recall = rec_metric.compute().item()
+                    avg_val_loss, avg_accuracy, avg_precision, avg_recall = eval_model(model, criterion, val_dataloader, device)
 
                     val_loss.append(avg_val_loss)
                     accuracy_list.append(avg_accuracy)
@@ -114,11 +113,17 @@ def train_model(epochs):
             with open("recall.json", "w") as f:
                 json.dump(recall_list, f)
 
-            torch.save(model.state_dict(), f"model_epoch_{epoch+1}.pt")
+            torch.save(model.state_dict(), "model.pt")
+
+    return model.state_dict()
+
                 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
+    model_con, data_con, train_con = config(), data_config(), train_config()
+    device =torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
     parser.add_argument("--epochs", type=int, default=10)
     args = parser.parse_args()
-    train_model(epochs = args.epochs)
+    train_model(epochs = args.epochs, model_con = model_con,data_con = data_con, train_con = train_con, device = device)
