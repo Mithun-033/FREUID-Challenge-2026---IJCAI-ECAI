9718a3be5504fb97b494a371e024262abed3ce0d
Mithun Kannaa <mithunkannaa609@gmail.com>
2026-07-06 17:33:20 +0530

    fixed fatal static resize bug

--- train_scripts/eval.py
+++ train_scripts/eval.py
@@ -6,34 +6,35 @@
 # from torchvision.io import decode_image
 # from tqdm import tqdm
 
-# from Model.Config import config
+# from Model.Config import config, data_config
 # from Model.model import Model
 
 # device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # model=Model(config)
 
-# state_dict=torch.load("model_epoch_10.pt", map_location=device)
+# state_dict=torch.load("best_model_1.pt", map_location=device)
 
-# state_dict={
-#     k.replace("_orig_mod.", ""):v
-#     for k,v in state_dict.items()
-# }
+# # state_dict={
+# #     k.replace("_orig_mod.", ""):v
+# #     for k,v in state_dict.items()
+# # }
 
 # model.load_state_dict(state_dict)
 # model.to(device)
 # model.eval()
+# conf = data_config()
 
 # transform=transforms.Compose([
 #     transforms.ConvertImageDtype(torch.float32),
-#     transforms.Resize((224,224)),
+#     transforms.Resize((conf.image_dim, conf.image_dim)),
 #     transforms.Normalize(
 #         mean=[0.485,0.456,0.406],
 #         std=[0.229,0.224,0.225]
 #     )
 # ])
 
-# image_dir=Path(r"C:\Users\mithu\Desktop\Mithun\FREUID Challenge 2026 - IJCAI-ECAI\the-freuid-challenge-2026-ijcai-ecai\public_test\public_test")
+# image_dir=Path(r"C:\Users\mithu\Desktop\Mithun\FREUID Challenge 2026 - IJCAI-ECAI\public_test\public_test")
 
 # image_paths=sorted([
 #     p for p in image_dir.iterdir()
@@ -58,11 +59,11 @@
 # df=pd.DataFrame(results)
 # df.to_csv("predictions_1.csv", index=False)
 
-# print(f"Saved {len(results)} predictions to predictions.csv")
+# print(f"Saved {len(results)} predictions to predictions_1.csv")
 
 import pandas as pd
 
-sample = pd.read_csv(r"C:\Users\mithu\Desktop\Mithun\FREUID Challenge 2026 - IJCAI-ECAI\the-freuid-challenge-2026-ijcai-ecai\sample_submission.csv")
+sample = pd.read_csv(r"C:\Users\mithu\Desktop\Mithun\FREUID Challenge 2026 - IJCAI-ECAI\sample_submission.csv")
 preds = pd.read_csv("predictions_1.csv")
 
 sample["label"]=sample["label"].astype(float)
@@ -72,4 +73,4 @@ preds=preds.set_index("id")
 
 sample.loc[preds.index,"label"]=preds["label"]
 
-sample.reset_index().to_csv("submission.csv", index=False)
+sample.reset_index().to_csv("submission_3.csv", index=False)
